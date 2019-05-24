2d27a8371f75d00095b4570fb1b55f96849537cb
gittheroadjack <ale_romero97@hotmail.com>
2019-05-24 14:10:28 +0200

    Modificando holamundo.java para quitar spoilers

--- src/holamundo.java
+++ src/holamundo.java
@@ -3,6 +3,5 @@ public class HolaMundo
 	public static void main(String[] args)
 	{
 		System.out.println("Hola mundo.");
-		System.out.println("Dice el refran que 4 ojos ve mas que dos, en este caso son 3");
 	}
 }
